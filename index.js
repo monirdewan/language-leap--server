73905eb72e62f26aa32778ec3f625bf636a3581b
Monir <hossinmonir681@gmail.com>
2023-06-13 23:19:09 +0600

    admin make api added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 //mongoDB
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.birk0vz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,18 +31,36 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const studentCollection = client.db('languageLeapDB').collection("student");
+    const usersCollection = client.db('languageLeapDB').collection("users");
 
-
-    app.post('/student', async(req, res)=>{
-        const student = req.body;
-        const query = {email: student.email}
-        const existingStudent = await studentCollection.findOne(query);
+//All Users
+    app.post('/users', async(req, res)=>{
+        const user = req.body;
+        const query = {email: user.email}
+        const existingStudent = await usersCollection.findOne(query);
         if(existingStudent){
             return res.send({message:"Student Account Already Exist"})
         }
-        const result = await studentCollection.insertOne(student);
+        const result = await usersCollection.insertOne(user);
         res.send(result)
+    })
+
+    app.get('/users', async(req, res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+    //admin
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const updateRole = {
+        $set:{
+          admin:'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(query, updateRole);
+      res.send(result)
     })
 
 
